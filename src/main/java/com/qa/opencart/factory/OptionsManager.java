f08e217eeb910666b223a3d33855581e009e41de
Vijay kumar <vijaym4ever@gmail.com>
2022-04-03 17:25:01 -0400

    added code to work to config file to adjust for limited resources

--- src/main/java/com/qa/opencart/factory/OptionsManager.java
+++ src/main/java/com/qa/opencart/factory/OptionsManager.java
@@ -18,6 +18,8 @@ public class OptionsManager {
 	
 	public ChromeOptions getChromeOptions() {
 		co = new ChromeOptions();
+		co.addArguments("--disable-dev-shm-usage");
+		co.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
 		if(Boolean.parseBoolean(prop.getProperty("headless"))) co.addArguments("--headless");
 		if(Boolean.parseBoolean(prop.getProperty("incognito"))) co.addArguments("--incognito");
 		/*
